6c700666d761bb26630921968e1b973dd7294d12
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-09 23:07:38 +1000

    Created ball class and object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
+ball = Ball()
 
 screen.listen()
 screen.onkeypress(fun=r_paddle.moveup, key='Up')
